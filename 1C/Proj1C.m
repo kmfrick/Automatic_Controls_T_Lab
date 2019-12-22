b52aaee8a109bd92302fd7cfabff7cfe255fc646
kmfrick <kmfrick98@gmail.com>
2019-12-22 20:25:46 +0100

    Updated report

--- 1C/Proj1C.m
+++ 1C/Proj1C.m
@@ -27,7 +27,7 @@ S_max = 0.01;
 omega_n = 200;
 A_n = 0.05 * pi / 180;
 B_n = 20;
-B_n_dB = 20 * log(20) / log(10);
+B_n_dB = 20 * log10(B_n);
 y_ref = pi/2;
 
 %% Matrici linearizzate
@@ -42,7 +42,8 @@ D = 0;
 %% Definizione funzione di trasferimento
 s=tf('s');
 [N,D]=ss2tf(A,B,C,D);
-G=tf(N,D);
+G_mat=tf(N,D);
+G = 1000 * (s+15) / (s^2 * (s^2 + 30 * s + 450));
 
 %% Definizione dell'intervallo di frequenze del diagramma di Bode
 omega_plot_min=10^(-2);
@@ -54,19 +55,28 @@ phi_m = xi * 100;
 omega_c_opt = 460 / (phi_m * T_a0);
 omega_c_stand = 460 / (phi_m * T_a1);
 
-%% Definizione del regolatore
+%% Definizione del regolatore per specifiche standard
+% Zero nell'origine per alzare la fase a valori accettabili
+margin(s * G); grid on;
+% Si ha margine di fase > 83 gradi per omega < 10
+gain_sta = 10^(-log10(abs(evalfr(s * G, 9.5 * 1i))));
+% Si aggiunge un polo lontano dall'origine per la fisica realizzabilità
+R_sta = gain_sta * s / (1 + s * 1e-5);
+L_sta = R_sta * G;
+F_sta = L_sta / (1 + L_sta);
+
+
+%% Definizione del regolatore ottimale
 gain = 3.4e-2;
-R_s = gain;
 T_lead_1 = 17.42;
 alpha_lead_1 = 5.741e-5;
 T_lead_2 = 0.05;
 alpha_lead_2 = 0.286;
 R_lead_1 = (1  + T_lead_1 * s) / (1 + alpha_lead_1 * T_lead_1 * s);
 R_lead_2 = (1  + T_lead_2 * s) / (1 + alpha_lead_2 * T_lead_2 * s);
-R = R_s * R_lead_1 * R_lead_2;
-R_no_opt = 0.23 * s / (1 + s * 1e-5);
-L_ = R_no_opt * G;
-F = L_ / (1 + L_);
+R_opt = gain * R_lead_1 * R_lead_2;
+L_opt = R_opt * G;
+F_opt = L_opt / (1 + L_opt);
 
 %% Diagramma di Bode del sistema in anello aperto
 figure;
@@ -78,26 +88,45 @@ patch([omega_n,omega_plot_max,omega_plot_max,omega_n],[-B_n_dB,-B_n_dB,100,100],
 margin(Mag, phase, omega); grid on;
 patch([omega_c_stand,omega_n, omega_n, omega_c_stand], [-180+phi_m,-180+phi_m, -360, -360], 'blue','FaceAlpha',0.5,'EdgeAlpha',0);
 
-
-%% Diagramma di Bode del sistema con regolatore
+%% Diagramma di Bode del sistema con regolatore standard
 figure;
 hold on;
 patch([omega_plot_min,omega_c_opt,omega_c_opt, omega_plot_min],[0, 0, -200, -200],'green','FaceAlpha',0.3,'EdgeAlpha',0);
 patch([omega_plot_min,omega_c_stand,omega_c_stand, omega_plot_min],[0, 0, -200, -200],'green','FaceAlpha',0.5,'EdgeAlpha',0);
 patch([omega_n,omega_plot_max,omega_plot_max,omega_n],[-B_n_dB,-B_n_dB,100,100],'red','FaceAlpha',0.5,'EdgeAlpha',0);
-[Mag, phase, omega] = bode(L_, {omega_plot_min, omega_plot_max});
+[Mag, phase, omega] = bode(L_sta, {omega_plot_min, omega_plot_max});
 margin(Mag, phase, omega);  
 grid on;
 patch([omega_c_stand,omega_n, omega_n, omega_c_stand], [-180+phi_m,-180+phi_m, -360, -360], 'blue','FaceAlpha',0.5,'EdgeAlpha',0);
 
-%% Risposta allo scalino del sistema in anello chiuso con regolatore
-step(F)
-stepinfo(F, 'SettlingTimeThreshold',0.01)
 
-%% Simulink
+%% Diagramma di Bode del sistema con regolatore ottimale
+figure;
+hold on;
+patch([omega_plot_min,omega_c_opt,omega_c_opt, omega_plot_min],[0, 0, -200, -200],'green','FaceAlpha',0.3,'EdgeAlpha',0);
+patch([omega_plot_min,omega_c_stand,omega_c_stand, omega_plot_min],[0, 0, -200, -200],'green','FaceAlpha',0.5,'EdgeAlpha',0);
+patch([omega_n,omega_plot_max,omega_plot_max,omega_n],[-B_n_dB,-B_n_dB,100,100],'red','FaceAlpha',0.5,'EdgeAlpha',0);
+[Mag, phase, omega] = bode(L_opt, {omega_plot_min, omega_plot_max});
+margin(Mag, phase, omega);  
+grid on;
+patch([omega_c_stand,omega_n, omega_n, omega_c_stand], [-180+phi_m,-180+phi_m, -360, -360], 'blue','FaceAlpha',0.5,'EdgeAlpha',0);
+
+%% Risposta allo scalino del sistema in anello chiuso con regolatore standard
+figure;
+hold on;
+step(F_sta);
+stepinfo(F_sta, 'SettlingTimeThreshold',0.01)
+
+%% Risposta allo scalino del sistema in anello chiuso con regolatore ottimale
+figure;
+hold on;
+step(F_opt);
+stepinfo(F_opt, 'SettlingTimeThreshold',0.01)
+
+%% Simulink (solo con regolatore ottimale)
 w_lin = W;
-lin_sim = sim('Simul1C');
 w_nonlin = W/8;
+lin_sim = sim('Simul1C');
 nonlin_sim = sim('NonLin1C');
 
 y_lin = lin_sim.get('y');
